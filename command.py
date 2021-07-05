5381858bc349b8bbffbf478543ecda429a4df72c
ETedward <edwardtian2000@gmail.com>
2021-07-05 13:12:36 -0400

    Add files via upload

--- command.py
+++ command.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 os.system('ls')
 df = pd.read_csv("input.csv")
-
+print(df)
 for i in range (len(df)):
-    print(i)
+    print(df.iloc[i,0])
     command = 'youtube-dl' + ' ' + df.iloc[i,0]
     os.system(command)
