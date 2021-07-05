0aeff7692fc1b67c6f1b2ec908971c909bd0f491
ETedward <edwardtian2000@gmail.com>
2021-07-05 12:45:53 -0400

    updated for csv file

--- command.py
+++ command.py
@@ -1,10 +1,9 @@
 import os
 import pandas as pd
 os.system('ls')
-df = pd.read_excel("input.xlsx")
+df = pd.read_csv("input.csv")
 
 for i in range (len(df)):
     print(i)
     command = 'youtube-dl' + ' ' + df.iloc[i,0]
     os.system(command)
-
