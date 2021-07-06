ef02a3fe02a3d47ad1cb18960fca84468f598fa6
ETedward <edwardtian2000@gmail.com>
2021-07-05 21:24:25 -0400

    Add files via upload
    debugged "output too long" error
    updated output string to include date and increment.

--- command.py
+++ command.py
@@ -5,5 +5,5 @@ df = pd.read_csv("input.csv")
 print(df)
 for i in range (len(df)):
     print(df.iloc[i,0])
-    command = 'youtube-dl' + ' ' + df.iloc[i,0]
+    command = 'youtube-dl -o\"%(upload_date)s.E' + str(i) + '.%(ext)s\"' + ' ' + df.iloc[i,0]
     os.system(command)
